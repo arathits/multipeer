c4d29b19d70ff792efa43f273b1726df8f5c60a4
arathits <8398arathi@gmail.com>
2019-06-14 19:09:22 +0530

    Complete cleaning of files and folders generated for network

--- clean.sh
+++ clean.sh
@@ -1,3 +1,8 @@
 docker-compose down
 docker rm $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
+
+rm -rf hfc-key-store
+rm -f supplychain.1.0
+rm -f supplychannel.block
+rm -f ./newjsfiles/login.txt
