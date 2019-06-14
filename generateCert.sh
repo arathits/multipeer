d5ca99a9f963e29572d595fe3a1f7a79ab0e55cf
arathits <8398arathi@gmail.com>
2019-06-14 23:03:07 +0530

    Doc update

--- generateCert.sh
+++ generateCert.sh
@@ -1,3 +1,8 @@
+#Cryptographic materials are created using cryptogen tool and placed in crypto-config folder.
+#Consumes crypto-config.yaml file ad input
+#This include certificates for each peer in the organisation
 ./cryptogen generate --config=crypto-config.yaml --output ./crypto-config
+
+#Generate genesis block and channel configuration as specified in configtx.yaml file
 ./configtxgen -profile SupplychainOrdererGenesis -outputBlock ./config/genesis.block
 ./configtxgen -profile SupplychainChannel -outputCreateChannelTx ./config/channel.tx -channelID "supplychannel"
